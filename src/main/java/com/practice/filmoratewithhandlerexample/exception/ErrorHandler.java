792408cdbc436e9b2ff08798c0c9d5ed0898bb9e
= <=>
2024-12-27 22:22:16 +0500

    init

--- src/main/java/com/practice/filmoratewithhandlerexample/exception/ErrorHandler.java
+++ src/main/java/com/practice/filmoratewithhandlerexample/exception/ErrorHandler.java
@@ -1,9 +1,11 @@
 package com.practice.filmoratewithhandlerexample.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -11,5 +13,15 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFound(NotFoundException e) { // обработка исключения
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleJakartaValidation(MethodArgumentNotValidException e) { // обработка аннотации @NotBlank
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> "%s: %s".formatted(fieldError.getField(), fieldError.getDefaultMessage()))
+                .findFirst()
+                .orElse("Валидация не пройдена");
+        return new ErrorResponse(message);
     }
 }
